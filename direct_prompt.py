Implement numerical keywords option Fix bug in 0-shot case and bug in generate_demonstration_file.py Update gitignore

direct_prompt.py
@@ -22,7 +22,7 @@ try:
 except ImportError:  # old huggingface does not have LlmaTimeForCausalLM
     from transformers import AutoTokenizer, MT5ForConditionalGeneration, AutoModelForSeq2SeqLM, AutoModelForCausalLM
 from few_shot_prompt_utility import format_prompt_from_demo_pairs, prompt_llm, evaluate_response_summaries, \
-    generate_tf_idf_keywords, generate_control_length, generate_focus_planning
+    generate_tf_idf_keywords, generate_control_length, generate_focus_planning, generate_numerical_keywords
 
 wandb.login(key='3138e1b24deb278ed045d0dedb39511d3a96245b')
 
@@ -32,8 +32,9 @@ parser.add_argument('--model', type=str, default='google/mt5-base', help='the na
 parser.add_argument('-k', type=int, default=1, help='the number of few-shot examples')
 parser.add_argument('--demonstration_file', type=str, default=None, help='the pre-generated demonstration file')
 parser.add_argument('--dataset', type=str, default='samsum', help='the dataset to evaluate on')
-parser.add_argument('--keywords', type=str, default=None, choices=['tfidf'], help='the types of keywords to use')
-parser.add_argument('--keyword_num', type=int, default=3, help='the number of keywords to use')
+parser.add_argument('--keywords', type=str, default=None, choices=['tfidf', 'numeric'],
+                    help='the types of keywords to use')
+parser.add_argument('--keyword_num', type=int, default=None, help='the number of keywords to use')
 parser.add_argument('--log', type=bool, default=True, help='whether to log the results to wandb')
 parser.add_argument('--control', type=str, default=None, choices=['length', 'entity', 'focus'],
                     help='the type of control')
@@ -43,6 +44,7 @@ parser.add_argument('--add_instruction', type=bool, default=False)
 parser.add_argument('--random_label', type=bool, default=False, help='whether to use random labels')
 # parse the arguments
 args = parser.parse_args()
+
 
 # set up log files
 logging.basicConfig(
@@ -70,6 +72,9 @@ if args.log:
             job_type='evaluation'
             )
 
+if args.keywords == 'numeric' and args.keyword_num is not None:
+    logging.warning('The number of keywords is not used for numerical keywords.')
+
 if args.demonstration_file is None and args.k == 0:
     test_dataset = load_dataset(args.dataset, split='test')
     run_id = 0
@@ -87,6 +92,8 @@ else:
 if args.control == 'entity':
     if args.keywords == 'tfidf':
         run_id2demo_pairs = generate_tf_idf_keywords(run_id2demo_pairs, args.keyword_num)
+    elif args.keywords == 'numeric':
+        run_id2demo_pairs = generate_numerical_keywords(run_id2demo_pairs)
     else:
         raise NotImplementedError
 elif args.control == 'length':
@@ -101,7 +108,8 @@ else:
 run_id2prompts, run_id2gold_summaries = format_prompt_from_demo_pairs(run_id2demo_pairs, args.model, args.replace_name,
                                                                       args.add_instruction,
                                                                       is_focus_planning=args.control == 'focus',
-                                                                      is_random_label=args.random_label)
+                                                                      is_random_label=args.random_label,
+                                                                      is_numerical_label=args.keywords == 'numeric')
 
 logging.info("load the model {}".format(args.model))
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')

few_shot_prompt_utility/prompt_formation.py
@@ -2,12 +2,15 @@ import random
 import numpy as np
 import nltk
 import string
+import spacy
 from nltk.corpus import stopwords
 import logging
 
 nltk.download('stopwords')
 nltk.download('punkt')
 STOP_WORDS = set(stopwords.words('english'))
+
+nlp = spacy.load("en_core_web_sm")
 
 
 def formulate_record_to_prompt_text(dialogue: str, model: str, summary: str = None, keyword_prompts: list = None,
@@ -81,7 +84,7 @@ def formulate_record_to_prompt_text(dialogue: str, model: str, summary: str = No
 
 def format_prompt_from_demo_pairs(run_id2demo_pairs: dict, model: str, is_replace_entity: bool = False,
                                   is_add_instruction: bool = False, is_focus_planning: bool = False,
-                                  is_random_label: bool = False):
+                                  is_random_label: bool = False, is_numerical_label: bool = False):
     """
     Format the prompt text from the demonstration pairs and save the prompt text to the file.
     Double newline is used to separate the prompt text for each dialogue.
@@ -91,15 +94,19 @@ def format_prompt_from_demo_pairs(run_id2demo_pairs: dict, model: str, is_replac
     """
     run_id2prompts = {}
     run_id2gold_summaries = {}
-    summaries = [d_[1]['summary'] for d in run_id2demo_pairs.values() for d_ in d.values()]
-    # convert summaries, a nested list, to a list
-    summaries = [s for s_ in summaries for s in s_]
+    try:
+        summaries = [d_[1]['summary'] for d in run_id2demo_pairs.values() for d_ in d.values()]
+        # convert summaries, a nested list, to a list
+        summaries = [s for s_ in summaries for s in s_]
+    except KeyError:
+        summaries = None
     for run_id, demo_pairs in run_id2demo_pairs.items():
         run_id2prompts[run_id] = []
         run_id2gold_summaries[run_id] = []
         for test_id, elements in demo_pairs.items():
             if len(elements) == 2:  # when no keywords
                 test_sample, demonstrations = elements
+                assert is_numerical_label is False
                 if is_add_instruction:
                     prompt = 'In this task, you are given an conversation. Your task is to summarize the ' \
                              'conversation.:\n'
@@ -151,40 +158,61 @@ def format_prompt_from_demo_pairs(run_id2demo_pairs: dict, model: str, is_replac
                     else:
                         prompt = ''
                     test_sample, demonstrations, keywords = elements
+                    assert len(keywords) > 0
                     keywords_id = 0
-                    for demo_dialogue, demo_summary in zip(demonstrations['dialogue'], demonstrations['summary']):
-                        keywords_id += 1
-                        if 'mt5' in model:
-                            # double \n
-                            # for space between demonstrations
-                            prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
-                                                                      is_focus_planning=is_focus_planning) + '\n' + '\n'
-                        else:
-                            if is_focus_planning:
+                    if len(demonstrations) != 0:
+                        for demo_dialogue, demo_summary in zip(demonstrations['dialogue'], demonstrations['summary']):
+                            keywords_id += 1
+                            if 'mt5' in model:
+                                # double \n
+                                # for space between demonstrations
                                 prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
-                                                                          keyword_prompts=keywords[keywords_id],
-                                                                          is_add_instruction= not is_add_instruction,
-                                                                          is_focus_planning=is_focus_planning) \
-                                          + '\n' \
-                                          + '\n'
+                                                                          is_focus_planning=is_focus_planning) + '\n' + '\n'
                             else:
-                                # use ntkl to extract not-stop-word keywords from demo_summary
-                                # and use them as the keywords for the prompt as the order of the keywords in the summary
-                                demo_keywords = nltk.word_tokenize(demo_summary)
-                                demo_keywords = [word for word in demo_keywords if
-                                                 word.isalpha() and word not in STOP_WORDS]
-                                keyword_num = len(keywords[0])
-                                demo_keywords_id = np.random.choice(range(len(demo_keywords)), min(keyword_num,
-                                                                                                   len(demo_keywords)),
-                                                                    replace=False)
-                                demo_keywords_id = sorted(demo_keywords_id)
-                                demo_keywords = [demo_keywords[i] for i in demo_keywords_id]
-                                prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
-                                                                          demo_keywords,
-                                                                          is_add_instruction= not is_add_instruction,
-                                                                          is_focus_planning=is_focus_planning) \
-                                          + '\n' \
-                                          + '\n'
+                                if is_focus_planning:
+                                    prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
+                                                                              keyword_prompts=keywords[keywords_id],
+                                                                              is_add_instruction= not is_add_instruction,
+                                                                              is_focus_planning=is_focus_planning) \
+                                              + '\n' \
+                                              + '\n'
+                                elif is_numerical_label:
+                                    # extract numerical information from demo_summary with spacy
+                                    demo_keywords = []
+                                    doc = nlp(demo_summary)
+                                    for ent in doc.ents:
+                                        if ent.label_ in ["TIME", "DATE", "QUANTITY", "PERCENT"]:
+                                            demo_keywords.append(ent.text)  # fixme: may not be numerical
+                                    if len(demo_keywords) == 0:
+                                        # extract the numbers from summary
+                                        demo_keywords = [word for word in nltk.word_tokenize(demo_summary)
+                                                         if word.isdigit()]
+                                    assert len(demo_keywords) > 0, 'demo summary does not cont' \
+                                                                   'ain numerical information: {}'.format(demo_summary)
+                                    prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
+                                                                              demo_keywords,
+                                                                              is_add_instruction= not is_add_instruction,
+                                                                              is_focus_planning=is_focus_planning) \
+                                              + '\n' \
+                                              + '\n'
+                                else:
+                                    # use ntkl to extract not-stop-word keywords from demo_summary
+                                    # and use them as the keywords for the prompt as the order of the keywords in the summary
+                                    demo_keywords = nltk.word_tokenize(demo_summary)
+                                    demo_keywords = [word for word in demo_keywords if
+                                                     word.isalpha() and word not in STOP_WORDS]
+                                    keyword_num = len(keywords[0])
+                                    demo_keywords_id = np.random.choice(range(len(demo_keywords)), min(keyword_num,
+                                                                                                       len(demo_keywords)),
+                                                                        replace=False)
+                                    demo_keywords_id = sorted(demo_keywords_id)
+                                    demo_keywords = [demo_keywords[i] for i in demo_keywords_id]
+                                    prompt += formulate_record_to_prompt_text(demo_dialogue, model, demo_summary,
+                                                                              demo_keywords,
+                                                                              is_add_instruction= not is_add_instruction,
+                                                                              is_focus_planning=is_focus_planning) \
+                                              + '\n' \
+                                              + '\n'
                     if 'mt5' in model:
                         prompt += formulate_record_to_prompt_text(test_sample['dialogue'], model,
                                                                   keyword_prompts=keywords[0],